c8ca08a55acff3f91d32c314408419ca589aee6c
Cumbie <bcumbie@una.edu>
2021-09-07 13:38:59 -0500

    changed joe joe to bob bob

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
 const express = require('express');
 const app = express();
+
+//using two new libraries
 const http = require('http');
 const fs = require('fs');
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
 
-    //res.send('<h3>Hello Node on Heroku</h3>');
+    // res.send('<h3>Hello Node on Heroku</h3><a href="https://github.com/barrycumbie/my-super-cool-awesome-project-app-heroku-node-thing" target="_blank">See the code!</a>');
     res.sendFile('/angular.html', { root: __dirname });
 
 
 });
 
-app.get('/user', function(req, res) {
+app.get('/user', function (req, res) {
 
     res.sendFile('/user.json', { root: __dirname });
 
 });
 
 
-app.get('/about', function(req, res) {
+app.get('/about', function (req, res) {
 
     res.sendFile('/README.md', { root: __dirname });
 
 });
 
-app.get('/file', function(req, res) {
+app.get('/file', function (req, res) {
 
-    fs.readFile('demofile1.html', function(err, data) {
+    fs.readFile('demofile1.html', function (err, data) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         return res.end();
     });
 
-    fs.appendFile('demofile1.html', 'Hello content!', function(err) {
+    fs.appendFile('demofile1.html', 'Hello content!', function (err) {
         if (err) throw err;
         console.log('Saved!');
     });
