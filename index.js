7997e6cbc1e6f17ab0001429923a012a022ccbd2
barry cumbie <barrycumbie@gmail.com>
2021-09-08 15:03:06 -0500

    updated my angular stuff

--- index.js
+++ index.js
@@ -5,36 +5,34 @@ const app = express();
 const http = require('http');
 const fs = require('fs');
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
 
     // res.send('<h3>Hello Node on Heroku</h3><a href="https://github.com/barrycumbie/my-super-cool-awesome-project-app-heroku-node-thing" target="_blank">See the code!</a>');
     res.sendFile('/angular.html', { root: __dirname });
-
-
 });
 
-app.get('/user', function (req, res) {
+app.get('/user', function(req, res) {
 
     res.sendFile('/user.json', { root: __dirname });
 
 });
 
 
-app.get('/about', function (req, res) {
+app.get('/about', function(req, res) {
 
     res.sendFile('/README.md', { root: __dirname });
 
 });
 
-app.get('/file', function (req, res) {
+app.get('/file', function(req, res) {
 
-    fs.readFile('demofile1.html', function (err, data) {
+    fs.readFile('demofile1.html', function(err, data) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         return res.end();
     });
 
-    fs.appendFile('demofile1.html', 'Hello content!', function (err) {
+    fs.appendFile('demofile1.html', 'Hello content!', function(err) {
         if (err) throw err;
         console.log('Saved!');
     });
@@ -42,4 +40,6 @@ app.get('/file', function (req, res) {
 
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8888, function() {
+    console.log('Listening on port 8888');
+});
